e2a12bce4a32b15b60d08e111ec86047fb30ccbf
Victor Fedorov <vpfedorov@bk.ru>
2022-07-17 20:22:30 +0300

    props lifting up

--- src/components/SongBox/SongBox.js
+++ src/components/SongBox/SongBox.js
@@ -2,16 +2,17 @@ import React from 'react';
 import SongImage from "../SongImage/SongImage";
 import ControlButtons from "../ControlButtons/ControlButtons";
 import SongInfo from "../SongInfo/SongInfo";
+import {useSelector} from "react-redux";
 
 const SongBox = () => {
+    const songId = useSelector(state => state.chosenSong.song);
+    const track = useSelector(state => state.trackList.tracks.filter(track => track.id === songId));
+
     return (
         <div className="songBox__container">
             {/*    Song image*/}
             <SongImage />
-            <SongInfo />
-            {/*    WaveForm*/}
-            {/*    Timeline*/}
-            {/*    ControlButtons*/}
+            <SongInfo track={track?.[0]} />
             <ControlButtons />
         </div>
     );
