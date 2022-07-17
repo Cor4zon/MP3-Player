bd37e8fb3e36c7a89b7aba64d414849aab967fdc
Victor Fedorov <vpfedorov@bk.ru>
2022-07-17 20:48:32 +0300

    time duration demonstration

--- src/components/ControlButtons/ControlButtons.js
+++ src/components/ControlButtons/ControlButtons.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCaretLeft, faPause, faPlay, faCaretRight } from "@fortawesome/free-solid-svg-icons";
@@ -11,6 +11,14 @@ library.add(faCaretRight);
 
 const ControlButtons = ({ track }) => {
     const [ isPlay, setIsPlay ] = useState(false);
+    const [ duration, setDuration ] = useState("");
+    const [ timePlay, setTimePlay ] = useState(0);
+
+    useEffect(() => {
+        const minutes = Math.floor(track?.audio.duration / 60)
+        const seconds = Math.floor(track?.audio.duration % 60)
+        setDuration(`${minutes}:${seconds}`)
+    }, [track]);
 
     const playButton = () => {
         if (isPlay) {
@@ -29,9 +37,11 @@ const ControlButtons = ({ track }) => {
         console.log("Next track");
     }
 
+
     return (
 
         <div className="controlButtons__container">
+            <h1>0:00</h1>
             <span style={{fontSize: 3 + 'em', color: 'red'}} onClick={prevTrack} >
                 <FontAwesomeIcon icon="fa-solid-6x fa-caret-left" />
             </span>
@@ -47,6 +57,7 @@ const ControlButtons = ({ track }) => {
             <span style={{fontSize: 3 + 'em', color: 'red'}} onClick={nextTrack} >
                 <FontAwesomeIcon icon="fa-solid fa-caret-right" />
             </span>
+            <h1>{duration}</h1>
         </div>
     );
 };
