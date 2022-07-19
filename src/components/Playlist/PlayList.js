ec91c5ebd1aaac2003980bb0781e8bbccdb0b047
Victor Fedorov <vpfedorov@bk.ru>
2022-07-19 18:57:25 +0300

    PlayList component refactoring

--- src/components/Playlist/PlayList.js
+++ src/components/Playlist/PlayList.js
@@ -1,19 +1,15 @@
-import {useState, useEffect} from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {changeSong} from "../../features/chosenSongSlice/chosenSongSlice";
+import "./PlayList.css";
 
-const Playlist = () => {
+const PlayList = () => {
     const trackList = useSelector(state => state.trackList.tracks);
     const dispatch = useDispatch()
 
-    const playTrack = (event) => {
-        console.log(event.target.className)
-        console.log(trackList);
+    const changeTrack = (event) => {
         for (let track of trackList) {
             if (track.id === +event.target.className) {
-                // track.audio.play();
                 dispatch(changeSong(+event.target.className))
-                console.log(`track duration⌛️: ${track.audio.duration}`)
                 break;
             }
         }
@@ -21,7 +17,7 @@ const Playlist = () => {
 
     const list = trackList.map((track, id) => {
         return (
-            <li key={id} className={track.id} onClick={playTrack}> {track.band} – {track.title}</li>
+            <li key={id} className={track.id} onClick={changeTrack}> {track.band} – {track.title}</li>
         )
     })
 
@@ -34,4 +30,4 @@ const Playlist = () => {
     );
 };
 
-export default Playlist;
+export default PlayList;
