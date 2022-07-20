be1fab3c8de98fb717a5d77288ccd9d2881a988d
Victor Fedorov <vpfedorov@bk.ru>
2022-07-20 15:07:54 +0300

    separate file for FontAwesome

---
+++ src/components/Icons/Icons.js
@@ -0,0 +1,4 @@
+import {faCaretLeft, faPause, faPlay, faCaretRight, faBars} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(faCaretLeft, faPlay, faPause, faCaretRight, faBars);
